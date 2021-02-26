1a4ebf693aba9c9c44aff1ec2997b1a1343fc744
William Junior <williamr.alvesjr@gmail.com>
2021-02-25 22:29:35 -0300

    Init tutorial

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 0.1f;
+    }
+        
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+    
+}
---
+++ Assets/TutorialController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField] private GameObject pause;
+    private Animator _animator;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        StartCoroutine(PauseFade());
+    }
+    
+    private IEnumerator PauseFade()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _animator.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(1f);
+        GetComponent<AudioSource>().Play();
+        pause.SetActive(true);
+        
+        yield return new WaitForSeconds(0.7f);
+        _animator.SetTrigger("End");
+        pause.SetActive(false);
+    }
+}
